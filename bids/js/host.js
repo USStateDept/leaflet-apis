9db7c431bf475cf04c1ef0ece00a64d10754dd56
brodydittemore-edip <dittemoremb@state.gov>
2014-05-01 23:12:18 +0000

    Initial commit of host.js

---
+++ bids/js/host.js
@@ -0,0 +1,16 @@
+var host = location.host;
+var loc = window.location.pathname;
+var site = loc.substring(0, loc.lastIndexOf('/')) + '/';
+
+function siteParams() {
+	gaID = "";
+	formSite = "";
+	
+	if (host === 'edip-maps.net') {
+		formSite = host;
+		gaID = "UA-42151027-1";
+	} else {
+		formSite = '10.47.115.214:8080';
+		gaID = "UA-42151027-2";
+	}
+}
